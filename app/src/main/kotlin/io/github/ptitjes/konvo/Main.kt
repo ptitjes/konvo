eceef2d7274d74fdcbde62ea86c740307f9b353d
Didier Villevalois <ptitjes@free.fr>
2025-06-11 19:03:55 +0200

    TEMP experiment with vision

--- app/src/main/kotlin/io/github/ptitjes/konvo/Main.kt
+++ app/src/main/kotlin/io/github/ptitjes/konvo/Main.kt
@@ -1,50 +1,73 @@
 package io.github.ptitjes.konvo
 
-import io.github.ptitjes.konvo.core.*
-import io.github.ptitjes.konvo.core.ai.koog.*
-import io.github.ptitjes.konvo.core.ai.mcp.*
-import io.github.ptitjes.konvo.frontend.discord.*
+import ai.koog.prompt.dsl.*
+import ai.koog.prompt.executor.ollama.client.*
+import ai.koog.prompt.llm.*
+import ai.koog.prompt.markdown.*
 import kotlinx.coroutines.*
-import kotlinx.io.files.*
 
 suspend fun main() = coroutineScope {
-    val configuration = KonvoAppConfiguration.readConfiguration(Path("config/konvo.json5"))
+    val client = OllamaClient()
 
-    val mcpServersManager = McpServersManager(
-        coroutineContext = coroutineContext,
-        specifications = configuration.mcp.servers,
+    val models = mapOf(
+        "granite3.2-vision:2b" to client.getModelOrNull("granite3.2-vision:2b")!!.toLLModel(),
+        "gemma3:4b" to client.getModelOrNull("gemma3:4b")!!.toLLModel(),
+        "llama3.2-vision:11b" to client.getModelOrNull("llama3.2-vision:11b")!!.toLLModel(),
     )
 
-    try {
-        mcpServersManager.startAndConnectServers()
+    val images = mapOf(
+        "Kotlin logo" to "/home/didier/Pictures/kotlin-logo.jpg",
+        "Avatar" to "/home/didier/Pictures/Avatar.png",
+        "Avatar Veganism" to "/home/didier/Pictures/Avatar-Veganism.png",
+        "Chasseur" to "/home/didier/Pictures/petition-chasse.jpg",
+        "Nupes" to "/home/didier/Pictures/NUPES/logo-nupes-complet.png",
+        "Borne" to "/home/didier/Pictures/Facebook/borne.jpg",
+        "French Polynesia" to "/home/didier/Pictures/Wallpapers/french-polynesia-1.jpg",
+    )
 
-        val konvo = startKonvo {
-            dataDirectory = configuration.dataDirectory
-
-            configuration.modelProviders.forEach { (name, configuration) ->
-                installModels(
-                    when (configuration) {
-                        is ModelProviderConfiguration.Ollama -> OllamaModelProvider(name, configuration.baseUrl)
-                    }
-                )
-            }
-
-            installPrompts(
-                McpPromptProvider(
-                    serversManager = mcpServersManager,
-                )
-            )
-
-            installTools(
-                McpToolProvider(
-                    serversManager = mcpServersManager,
-                    permissions = configuration.mcp.toolPermissions,
-                )
-            )
+    val results = models.mapValues { (modelName, model) ->
+        images.mapValues { (imageName, image) ->
+            println("- $modelName - $imageName")
+            client.queryImageContent(image, model)
         }
-
-        konvo.discordBot(configuration.discord.token)
-    } finally {
-        mcpServersManager.disconnectAndStopServers()
     }
+    println()
+    println()
+
+    val reversedResults = results
+        .flatMap { (model, modelResults) -> modelResults.entries.map { (image, result) -> image to (model to result) } }
+        .groupBy { (image, _) -> image }
+        .mapValues { (_, imageResults) -> imageResults.associate { (_, imageResult) -> imageResult } }
+
+    println(
+        markdown {
+            for ((image, imageResults) in reversedResults) {
+                h1(image)
+                newline()
+
+                for ((model, result) in imageResults) {
+                    h2(model)
+                    newline()
+                    +result
+                    newline()
+                }
+                br()
+            }
+        }
+    )
+
+}
+
+private suspend fun OllamaClient.queryImageContent(
+    image: String,
+    model: LLModel
+): String {
+    val prompt = prompt("test") {
+        user {
+            text("Please describe this image.")
+            attachments { image(image) }
+        }
+    }
+    val responses = execute(prompt, model)
+    return responses.joinToString("\n") { it.content }
 }
