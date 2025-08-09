145470e7638b4243617484fa63cc11ec8f5670f1
Didier Villevalois <ptitjes@free.fr>
2025-08-10 00:46:00 +0200

    clean(core): remove forgotten debug logging

--- konvo-core/src/commonMain/kotlin/io/github/ptitjes/konvo/core/ai/mcp/McpServerHandler.kt
+++ konvo-core/src/commonMain/kotlin/io/github/ptitjes/konvo/core/ai/mcp/McpServerHandler.kt
@@ -6,14 +6,8 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.sse.*
 import io.modelcontextprotocol.kotlin.sdk.*
 import io.modelcontextprotocol.kotlin.sdk.client.*
-import io.modelcontextprotocol.kotlin.sdk.shared.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.*
 import kotlinx.io.*
-import kotlinx.io.files.*
-import kotlinx.serialization.*
-import kotlinx.serialization.json.*
-import kotlin.concurrent.atomics.*
 import kotlin.coroutines.*
 import kotlin.time.Duration.Companion.seconds
 
@@ -67,13 +61,9 @@ internal class McpServerHandler(
 
         try {
             val transport = when (transportSpecification) {
-                is TransportSpecification.Stdio if (name == "web") -> CustomStdioClientTransport(
-                    input = process!!.inputStream.asSource().log(name).buffered(),
-                    output = process!!.outputStream.asSink().log(name).buffered()
-                )
                 is TransportSpecification.Stdio -> StdioClientTransport(
-                    input = process!!.inputStream.asSource().log(name).buffered(),
-                    output = process!!.outputStream.asSink().log(name).buffered()
+                    input = process!!.inputStream.asSource().buffered(),
+                    output = process!!.outputStream.asSink().buffered()
                 )
 
                 is TransportSpecification.Sse -> SseClientTransport(
@@ -126,179 +116,3 @@ private suspend fun spawnProcess(command: List<String>, environment: Map<String,
     withContext(Dispatchers.IO) {
         ProcessBuilder(command).apply { environment?.let { environment().putAll(it) } }.start()
     }
-
-fun RawSource.log(name: String): RawSource = LoggerSource(this, name)
-
-class LoggerSource(private val delegate: RawSource, name: String) : RawSource {
-    private val log = SystemFileSystem.sink(Path("./$name-input-client.log"))
-
-    override fun readAtMostTo(sink: Buffer, byteCount: Long): Long {
-        val buffer = Buffer()
-        val read = delegate.readAtMostTo(buffer, byteCount)
-        if (read == -1L) return -1L
-        log.write(buffer.copy(), read)
-        log.flush()
-        buffer.readAtMostTo(sink, read)
-        return read
-    }
-
-    override fun close() {
-        val closedBuffer = Buffer().apply { writeString("CLOSED\n") }
-        log.write(closedBuffer, closedBuffer.size)
-        log.flush()
-        log.close()
-        delegate.close()
-    }
-}
-
-fun RawSink.log(name: String): RawSink = LoggerSink(this, name)
-
-class LoggerSink(private val delegate: RawSink, name: String) : RawSink {
-    private val log = SystemFileSystem.sink(Path("./$name-output-client.log"))
-
-    override fun write(source: Buffer, byteCount: Long) {
-        log.write(source.copy(), byteCount)
-        log.flush()
-        delegate.write(source, byteCount)
-    }
-
-    override fun flush() {
-        delegate.flush()
-    }
-
-    override fun close() {
-        log.close()
-        delegate.close()
-    }
-}
-
-@OptIn(ExperimentalAtomicApi::class)
-public class CustomStdioClientTransport(
-    private val input: Source,
-    private val output: Sink
-) : AbstractTransport() {
-    private val logger = KotlinLogging.logger {}
-    private val ioCoroutineContext: CoroutineContext = Dispatchers.IO
-    private val scope by lazy {
-        CoroutineScope(ioCoroutineContext + SupervisorJob())
-    }
-    private var job: Job? = null
-    private val initialized: AtomicBoolean = AtomicBoolean(false)
-    private val sendChannel = Channel<JSONRPCMessage>(Channel.UNLIMITED)
-    private val readBuffer = ReadBuffer()
-
-    override suspend fun start() {
-        if (!initialized.compareAndSet(false, true)) {
-            error("StdioClientTransport already started!")
-        }
-
-        logger.debug { "Starting StdioClientTransport..." }
-
-        val outputStream = output.buffered()
-
-        job = scope.launch(CoroutineName("StdioClientTransport.IO#${hashCode()}")) {
-            val readJob = launch {
-                logger.debug { "Read coroutine started." }
-                try {
-                    input.use {
-                        logger.debug { "----------------- 1" }
-                        while (isActive) {
-                            logger.debug { "----------------- 2" }
-                            val buffer = Buffer()
-                            val bytesRead = input.readAtMostTo(buffer, 8192)
-                            logger.debug { "----------------- 3 > $bytesRead" }
-                            if (bytesRead == -1L) break
-                            if (bytesRead > 0L) {
-                                logger.debug { "----------------- 4" }
-                                readBuffer.append(buffer.readByteArray())
-                                logger.debug { "----------------- 5" }
-                                processReadBuffer()
-                                logger.debug { "----------------- 6" }
-                            }
-                            logger.debug { "----------------- 7" }
-                        }
-                        logger.debug { "----------------- 8" }
-                    }
-                } catch (e: Exception) {
-                    _onError.invoke(e)
-                    logger.error(e) { "Error reading from input stream" }
-                }
-            }
-
-            val writeJob = launch {
-                logger.debug { "Write coroutine started." }
-                try {
-                    sendChannel.consumeEach { message ->
-                        val json = serializeMessage(message)
-                        outputStream.writeString(json)
-                        outputStream.flush()
-                    }
-                } catch (e: Throwable) {
-                    if (isActive) {
-                        _onError.invoke(e)
-                        logger.error(e) { "Error writing to output stream" }
-                    }
-                } finally {
-                    output.close()
-                }
-            }
-
-            readJob.join()
-            writeJob.cancelAndJoin()
-            _onClose.invoke()
-        }
-    }
-
-    override suspend fun send(message: JSONRPCMessage) {
-        if (!initialized.load()) {
-            error("Transport not started")
-        }
-
-        sendChannel.send(message)
-    }
-
-    override suspend fun close() {
-        if (!initialized.compareAndSet(true, false)) {
-            error("Transport is already closed")
-        }
-        job?.cancelAndJoin()
-        input.close()
-        output.close()
-        readBuffer.clear()
-        sendChannel.close()
-        _onClose.invoke()
-    }
-
-    private suspend fun processReadBuffer() {
-        logger.debug { "============== 1" }
-        while (true) {
-            logger.debug { "============== 2" }
-            val msg = readBuffer.readMessage() ?: break
-            logger.debug { "============== 3" }
-            try {
-                _onMessage.invoke(msg)
-            } catch (e: Throwable) {
-                _onError.invoke(e)
-                logger.error(e) { "Error processing message." }
-            }
-            logger.debug { "============== 4" }
-        }
-        logger.debug { "============== 5" }
-    }
-}
-
-internal fun serializeMessage(message: JSONRPCMessage): String {
-    return McpJson.encodeToString(message) + "\n"
-}
-
-@OptIn(ExperimentalSerializationApi::class)
-@PublishedApi
-internal val McpJson: Json by lazy {
-    Json {
-        ignoreUnknownKeys = true
-        encodeDefaults = true
-        isLenient = true
-        classDiscriminatorMode = ClassDiscriminatorMode.NONE
-        explicitNulls = false
-    }
-}
