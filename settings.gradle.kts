b81abbe596a0434aad2ac0dedb9fc45b32ff4203
Didier Villevalois <ptitjes@free.fr>
2025-08-22 22:05:29 +0200

    build: temporarily deactivate discord bot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,10 +14,10 @@ plugins {
 
 include(":konvo-core")
 include(":konvo-frontend-compose")
-include(":konvo-frontend-discord")
+//include(":konvo-frontend-discord")
 include(":konvo-mcp-prompt-collection")
 include(":konvo-mcp-web-tools")
-include(":apps:discord-bot")
+//include(":apps:discord-bot")
 include(":apps:desktop-app")
 
 rootProject.name = "konvo"
