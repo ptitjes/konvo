c7ac0579eb77fc05751091e6d4c70a73f30128cd
Didier Villevalois <ptitjes@free.fr>
2026-03-16 10:40:50 +0100

    chore: Remove discord-bot app and related frontend discord modules

--- konvo-frontend-discord/src/commonMain/kotlin/io/github/ptitjes/konvo/frontend/discord/DiscordConstants.kt
+++
@@ -1,7 +0,0 @@
-package io.github.ptitjes.konvo.frontend.discord
-
-import dev.kord.common.*
-
-internal object DiscordConstants {
-    val KonvoColor = Color(177, 37, 234)
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,10 +14,8 @@ plugins {
 
 include(":konvo-core")
 include(":konvo-frontend-compose")
-//include(":konvo-frontend-discord")
 include(":konvo-mcp-prompt-collection")
 include(":konvo-mcp-web-tools")
-//include(":apps:discord-bot")
 include(":apps:desktop-app")
 
 rootProject.name = "konvo"
