271bdf65bed36a0c4d08f026bc7dddb3cf98dde3
Didier Villevalois <ptitjes@free.fr>
2026-04-05 17:20:03 +0200

    refactor(roleplay): rework and simplify lorebook management

--- plugins/konvo-plugin-roleplay/src/commonMain/kotlin/io/github/ptitjes/konvo/plugin/roleplay/LorebookManager.kt
+++ plugins/konvo-plugin-roleplay/src/commonMain/kotlin/io/github/ptitjes/konvo/plugin/roleplay/LorebookManager.kt
@@ -1,17 +1,31 @@
 package io.github.ptitjes.konvo.plugin.roleplay
 
 import kotlinx.coroutines.flow.*
+import kotlinx.io.files.*
 
 /**
  * Dedicated manager for lorebooks.
  */
 interface LorebookManager {
-    /** A flow emitting the list of available lorebooks. */
-    val lorebooks: Flow<List<Lorebook>>
+    /** A flow emitting any errors encountered by the manager. */
+    val error: SharedFlow<String>
+
+    /** A flow emitting the set of available lorebooks. */
+    val lorebooks: SharedFlow<Set<Lorebook>>
+
+    /**
+     * Adds a lorebook from a given [sourcePath].
+     */
+    fun add(sourcePath: Path)
+
+    /**
+     * Deletes a [lorebook].
+     */
+    fun delete(lorebook: Lorebook)
 }
 
 /**
- * Retrieve a lorebook by its name from the current emission or throw if not found.
+ * Retrieves a lorebook by its id or throws if not found.
  */
 suspend fun LorebookManager.withId(id: String): Lorebook =
     lorebooks.first().firstOrNull { it.id == id } ?: error("Lorebook not found: $id")
